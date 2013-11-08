84cf1db0d8a4fe1cdd894c2b5b7175e26182b312
Josh Nussbaum <joshnuss@gmail.com>
2013-11-07 19:51:59 -0500

    Moves #j & #w methods from index.html to app/app.rb. Adds #window and #jquery alias

--- app/app.rb
+++ app/app.rb
@@ -1,1 +1,15 @@
-# This is the entrypoint file for webruby
+Kernel.define_method :jquery do |selector|
+  MrubyJs.window.jQuery(selector)
+end
+
+Kernel.define_method :j do |selector|
+  jquery(selector)
+end
+
+Kernel.define_method :window do
+  MrubyJs.window
+end
+
+Kernel.define_method :w do
+  window
+end
